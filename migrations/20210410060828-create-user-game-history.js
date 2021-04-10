c52f7872d880fac898e2947e48a21b55d606e748
Tito-Rollis <ouvalres@gmail.com>
2021-04-10 13:09:22 +0700

    add model and migrate user_game_history

---
+++ migrations/20210410060828-create-user-game-history.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('user_game_histories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      skor: {
+        type: Sequelize.INTEGER
+      },
+      play_time: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('user_game_histories');
+  }
+};
