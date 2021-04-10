c801f7bcfa4c347131ddd0d0c2e8fafa8200dfe1
Tito-Rollis <ouvalres@gmail.com>
2021-04-10 13:22:56 +0700

    add seeder for user_games

---
+++ seeders/20210410061052-user.js
@@ -0,0 +1,36 @@
+"use strict";
+
+module.exports = {
+	up: async (queryInterface, Sequelize) => {
+		/**
+		 * Add seed commands here.
+		 *
+		 * Example:
+		 * await queryInterface.bulkInsert('People', [{
+		 *   name: 'John Doe',
+		 *   isBetaMember: false
+		 * }], {});
+		 */
+		await queryInterface.bulkInsert(
+			"user_games",
+			[
+				{
+					username: "tito",
+					password: "qwerty",
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+			],
+			{}
+		);
+	},
+
+	down: async (queryInterface, Sequelize) => {
+		/**
+		 * Add commands to revert seed here.
+		 *
+		 * Example:
+		 * await queryInterface.bulkDelete('People', null, {});
+		 */
+	},
+};
